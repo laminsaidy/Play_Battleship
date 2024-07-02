8f32eef22474e9839d6d7711fe87001e9314076f
Lamin Saidy <nimalydias@gmail.com>
2024-07-02 15:27:22 +0000

    Create an if/else statement to check if a Ship is either hit, miss or destroyed

--- run.py
+++ run.py
@@ -4,12 +4,25 @@ from random import randint
 
 print("Welcome to my Battlefield Game")
 print("            Ships ")
-print(" 0  1 2 3 4 5 6")
+print("     0  1  2  3  4  5  6  7  8  9")
 
+hit = [19,20,]
+miss = [6,10,22,26]
+destroy = [21]
+counter = 0 
+# create a nested for loop that prints "_" in a row and a nested if/else statement to check if ship is hit, miss or destroyed
+for x in range(10):
+   row = ""
+   for y in range(10):
+      sym = " _ "
+      if counter in miss:
+         sym = " x "
+      elif counter in hit:
+         sym = " o "
+      elif counter in destroy:
+         sym = " @ "
 
-# create a for loop that prints each number of rows in a board 
-for x in range(7):
-    print(x, "_ "*7)
-
-
+      row = row + sym
+      counter = counter + 1
+   print(x," ",row)
    
