726e54f7cbce93f310e3a81237b17798ecb90370
Lamin Saidy <nimalydias@gmail.com>
2024-07-02 14:37:11 +0000

    Create a board using a for loop

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import random package to create random board 
+from random import randint
+
+
+print("Welcome to my Battlefield Game")
+print("            Ships ")
+print(" 0  1 2 3 4 5 6")
+
+
+# create a for loop that prints each number of rows in a board 
+for x in range(7):
+    print(x, "_ "*7)
+
+
+   
