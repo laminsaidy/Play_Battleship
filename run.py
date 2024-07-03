751378fdb3d67b3daf2020a588ac69dd61805157
Lamin Saidy <nimalydias@gmail.com>
2024-07-03 16:38:20 +0000

    Edit on game board

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 # import random package to create random board 
 from random import randint
 
+hit = []
+miss = []
+destroy = [] 
+ships = [[6,16,26],[45,46,47]]    
+
 print("Welcome to Battleship!")
 player1_name = input("Please enter your name: ")
 print(f"Hello {player1_name}! Let's get it on.")
 
-def board (hit, miss, destroy):
+def board ():
    # prints gameboard 
    print("            Ships ")
    print("     0  1  2  3  4  5  6  7  8  9")
@@ -13,31 +18,27 @@ def board (hit, miss, destroy):
    counter = 0
 # Create a nested for loop that prints "." in a row and a nested if/else statement to check if ship is hit, miss or destroyed
    for x in range(10):   
-       row = ""
-       for y in range(9):
+      row = ""
+      for y in range(10):
          symb = " . "
-       if counter in miss:
-         symb = " x "
-       elif counter in hit:
-         symb = " o "
-       elif counter in destroy:
-         symb = " 0 "
+         if counter in miss:
+            symb = " o "
+         elif counter in hit:
+            symb = " x "
+         elif counter in destroy:
+            symb = " 0 "
 
-       row = row + symb
-       counter = counter + 1
-       print(x," ",row*10)
+         row = row + symb
+         counter = counter + 1
+      print(x," ",row)
 
-hit = []
-miss = []
-destroy = [] 
-    
 
 # Create a function to get shot input from user
 def take_a_shot(total_guesses):
    
 # Create a while loop to check if shot is correct or incorrect
-     alright = "no"
-     while alright == "no":
+      alright = False
+      while not alright:
 # Create a try and except method incase of an error to avoid a crash
          try:
             shot = input("place your shot please")
@@ -48,49 +49,40 @@ def take_a_shot(total_guesses):
             elif shot in total_guesses:
                print("That's invalid! It has been used before")
             else:
-               alright = "yes"
-               break
+               alright = True
          except:
-               print("That's invalid! Please try again")
-         return shot
+            print("That's invalid! Please try again")
+      return shot
 
 
 
 #Create a function to validate a shot of hit, miss or destroy Ship
-def validate_shot(shot,ship1,ship2,hit,miss,destroy):
+def validate_shot(shot):
 
 # An if statement to validate if it's a hit, miss or on a ship
-   if shot in ship1:
-      ship1.remove(shot)
-      if len(ship1) > 0:
+   for ship in ships:
+      if shot in ship:
+         ship.remove(shot)
          hit.append(shot)
       else:
-         destroy.append(shot)
-   elif shot in ship2:
-      ship2.remove(shot)
-      if len(ship2) > 0:
-         hit.append(shot)
-      else:
-         destroy.append(shot)
-   else:
-      miss.append(shot)
+         miss.append(shot)
+      
 
-   return ship1, ship2, hit, miss, destroy
-
-ship1 = [6,16,26]
-ship2 = [45,46,47]
 
 
 
 # Condition for how many shots are allowed
 for i in range (10):
    total_guesses = hit + miss + destroy
-   ship1, hit, miss, destroy = validate_shot = (ship1, hit, miss, destroy)
+   board()
    shot = take_a_shot(total_guesses)
-   board(hit, miss, destroy)
+   validate_shot(shot)
 
+ 
+   
+  
 # Condition to end the Game and break out of a loop
-   if len(ship1) < 1 and len(ship2) < 1:
+   if len(ships) < 1:
       print("You are the winner")
       break
 print("End of Game")
