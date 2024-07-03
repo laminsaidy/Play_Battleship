cfa79c19d817409d696984cf1f365fc2ea288525
Lamin Saidy <nimalydias@gmail.com>
2024-07-03 19:37:36 +0000

    Create a function validateShotComputer to allow computer to take a shot

--- run.py
+++ run.py
@@ -1,94 +1,96 @@
 
-# import random package to create random board 
+# Import random package to create random board 
 from random import randint
 
-hit = []
-miss = []
-destroy = [] 
-ships = [[6,16,26],[45,46,47]]    
+player_hits = []
+player_misses = []
+computer_hits = []
+computer_misses = []
+player_ship = [6, 16, 26]    
+computer_ship = [45, 46, 47]
 
 print("Welcome to Battleship!")
 player1_name = input("Please enter your name: ")
 print(f"Hello {player1_name}! Let's get it on.")
 
-def board ():
-   # prints gameboard 
+def board():
    print("            Ships ")
    print("     0  1  2  3  4  5  6  7  8  9")
 
    counter = 0
-# Create a nested for loop that prints "." in a row and a nested if/else statement to check if ship is hit, miss or destroyed
    for x in range(10):   
       row = ""
       for y in range(10):
          symb = " . "
-         if counter in miss:
-            symb = " o "
-         elif counter in hit:
-            symb = " x "
-         elif counter in destroy:
-            symb = " 0 "
+         if counter in player_misses or counter in computer_misses:
+            symb = " O "
+         elif counter in player_hits:
+            symb = " P "
+         elif counter in computer_hits:
+            symb = " C "
+         row += symb
+         counter += 1
+      print(x, " ", row)
 
-         row = row + symb
-         counter = counter + 1
-      print(x," ",row)
+def takeAShot(total_guesses):
+   alright = False
+   while not alright:
+      try:
+         shot = input("Place your shot please: ")
+         shot = int(shot)
+         if shot < 0 or shot > 99:
+            print("OOOPS! That's incorrect. Please try again.")
+         elif shot in total_guesses:
+            print("That's invalid! It has been used before.")
+         else:
+            alright = True
+      except ValueError:
+         print("That's invalid! Please try again.")
+   return shot
 
+def computerShot(total_guesses):
+   shot = randint(0, 99)
+   while shot in total_guesses:
+      shot = randint(0, 99)
+   return shot
 
-# Create a function to get shot input from user
-def take_a_shot(total_guesses):
-   
-# Create a while loop to check if shot is correct or incorrect
-      alright = False
-      while not alright:
-# Create a try and except method incase of an error to avoid a crash
-         try:
-            shot = input("place your shot please")
-            shot = int(shot)
-            if shot <0 or shot >99:
-               print("OOOPS! That's incorrect. Please try again")
-# Check if player used a recent guess
-            elif shot in total_guesses:
-               print("That's invalid! It has been used before")
-            else:
-               alright = True
-         except:
-            print("That's invalid! Please try again")
-      return shot
+def validateShotPlayer(shot):
+   global player_ship
+   if shot in player_ship:
+      player_ship.remove(shot)
+      player_hits.append(shot)
+   else:
+      player_misses.append(shot)
 
+def validateShotComputer(shot):
+   global computer_ship
+   if shot in computer_ship:
+      computer_ship.remove(shot)
+      computer_hits.append(shot)
+   else:
+      computer_misses.append(shot)
 
-
-def validate_shot(shot):
-   global ships
-   is_hit = False
-# An if statement to validate if it's a hit, miss or on a ship
-   for ship in ships:
-      if shot in ship:
-         ship.remove(shot)
-         hit.append(shot)
-         is_hit = True
-         if not ship:  # If ship is completely destroyed
-            destroy.append(ship)
-            ships.remove(ship)
-         break
-   if not is_hit:
-      miss.append(shot)
-
-
-
-
-
-# Condition for how many shots are allowed
-for i in range (10):
-   total_guesses = hit + miss + destroy
+for i in range(20):
+   total_guesses_player = player_hits + player_misses
+   total_guesses_computer = computer_hits + computer_misses
    board()
-   shot = take_a_shot(total_guesses)
-   validate_shot(shot)
-
- 
    
-  
-# Condition to end the Game and break out of a loop
-   if len(ships) < 1:
-      print("You are the winner")
+   # Player's turn
+   shot = takeAShot(total_guesses_player)
+   validateShotPlayer(shot)
+   
+   # Computer's turn
+   shot = computerShot(total_guesses_computer)
+   validateShotComputer(shot)
+
+   print(f"Computer shot at position {shot}")
+
+   # Condition to end the Game and break out of a loop
+   if not player_ship:
+      print("You are the winner!")
       break
-print("End of Game")
+   elif not computer_ship:
+      print("Computer is the winner!")
+      break
+else:
+   print("End of Game")
